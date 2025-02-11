8cb779128bf0f1043d646c08d84847722f0adba7
aaarf404 <alexmrf6439@gmail.com>
2025-02-11 13:46:21 -0800

    Update global.js

--- global.js
+++ global.js
@@ -3,3 +3,18 @@ console.log('IT’S ALIVE!');
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
+
+// global.js
+console.log("IT’S ALIVE!");
+
+const navLinks = $$("nav a");
+
+let currentLink = navLinks.find(a =>
+  a.host === location.host && a.pathname === location.pathname
+);
+
+if (currentLink) {
+  currentLink.classList.add("current");
+}
+
+
