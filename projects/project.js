8775ba39935d89b3a52d6cc85ef97000bbfa928f
aaarf404 <alexmrf6439@gmail.com>
2025-02-18 15:10:21 -0800

    Update project.js

--- projects/project.js
+++ projects/project.js
@@ -1,1 +1,7 @@
-import { fetchJSON, renderProjects } from '../global.js';
+import { fetchJSON, renderProjects } from "../global.js";
+
+(async function() {
+  const data = await fetchJSON("../projects.json");  // Up one level
+  const container = document.querySelector(".projects");
+  renderProjects(data, container, "h2");
+})();
