32715a61dda59df6790f5b3f1638aac2ec09f77a
aaarf404 <alexmrf6439@gmail.com>
2025-04-18 22:18:47 -0700

    Update global.js

--- global.js
+++ global.js
@@ -1,43 +1,38 @@
-console.log('IT’S ALIVE!');
-
+console.log("IT’S ALIVE!");
 function $$(selector, context = document) {
-    return Array.from(context.querySelectorAll(selector));
+  return Array.from(context.querySelectorAll(selector));
 }
 
-let navLinks = $$("nav a");
-
-let currentLink = navLinks.find(
-    (a) => a.host === location.host && a.pathname === location.pathname,
-  );
-
-  if (currentLink) {
-    // or if (currentLink !== undefined)
-    currentLink.classList.add('current');
-  }
-
-  const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
-  ? "/"                  // Local server
-  : "/website/";         // GitHub Pages repo name
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+  ? "/"                // Local server
+  : "/portfolio/";     // GitHub Pages repo name
 
 let pages = [
-    { url: "", title: "Home" },
-    { url: "projects/", title: "Projects" },
-    { url: "resume/", title: "Resume" },
-    { url: "contact/", title: "Contact" },
-    { url: "https://github.com/aaarf404", title: "Github" }
-  ];
+  { url: "", title: "Home" },
+  { url: "projects/", title: "Projects" },
+  { url: "resume/", title: "Resume" },
+  { url: "contact/", title: "Contact" },
+  { url: "https://github.com/aaarf404", title: "Github" }
+];
 
 let nav = document.createElement("nav");
 document.body.prepend(nav);
 
 for (let p of pages) {
-    let url = p.url;
-    let title = p.title;
-  
-    if (!url.startsWith("http")) {
-      url = BASE_PATH + url;
-    }
-  
-nav.insertAdjacentHTML("beforeend", `<a href="${url}">${title}</a>`);
+  let url = p.url.startsWith("http") ? p.url : BASE_PATH + p.url;
+  let title = p.title;
+
+  let a = document.createElement("a");
+  a.href = url;
+  a.textContent = title;
+
+  if (a.host === location.host && a.pathname === location.pathname) {
+    a.classList.add("current");
   }
-  
+
+  if (a.host !== location.host) {
+    a.target = "_blank";
+  }
+
+  nav.append(a);
+}
