fa6ceb81a45abbc5c351cc22376d8d226df81c11
aaarf404 <alexmrf6439@gmail.com>
2025-04-18 22:42:55 -0700

    fix error

--- global.js
+++ global.js
@@ -14,20 +14,21 @@ let pages = [
 
 const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
   ? "/" 
-  : "/portfolio/"; // ✅ important!
+  : "/portfolio/"; 
 
 let nav = document.createElement('nav');
 document.body.prepend(nav);
 
 for (let p of pages) {
-  let url = p.url.startsWith('http') ? p.url : BASE_PATH + p.url;
+  let url = p.url
   let title = p.title;
+
+  url = !url.startsWith('http') ? BASE_PATH + url : url;
 
   let a = document.createElement("a");
   a.href = url;
   a.textContent = title;
 
-  // Highlight current page
   a.classList.toggle(
     'current',
     a.host === location.host && a.pathname === location.pathname
