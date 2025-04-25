56240c57775ec4d3131ebba38f9067daf752c1d2
aaarf404 <alexmrf6439@gmail.com>
2025-04-25 02:04:47 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+import { fetchJSON, renderProjects } from './global.js';
+
+async function loadLatestProjects() {
+  const projects = await fetchJSON('./lib/projects.json');
+  const latestProjects = projects.slice(0, 3);
+
+  const projectsContainer = document.querySelector('.projects');
+
+  renderProjects(latestProjects, projectsContainer, 'h2');
+}
+
+loadLatestProjects();
