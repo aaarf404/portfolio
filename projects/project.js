abc818811b704da0da1b2cda1718a729334ad3c5
aaarf404 <alexmrf6439@gmail.com>
2025-04-25 01:19:02 -0700

    finish step 1

---
+++ projects/project.js
@@ -0,0 +1,13 @@
+import { fetchJSON, renderProjects } from '../global.js';
+
+const projectsContainer = document.querySelector('.projects');
+
+async function init() {
+  const projects = await fetchJSON('../lib/projects.json');
+  renderProjects(projects, projectsContainer, 'h2');
+}
+
+init();
+
+const titleElement = document.querySelector('.projects-title');
+titleElement.textContent = `${projects.length} Projects`;
