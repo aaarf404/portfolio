7ade14f1142be82d77b4864a77fbd083dce57a30
aaarf404 <alexmrf6439@gmail.com>
2025-04-26 23:32:32 -0700

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -3,7 +3,7 @@ import { fetchJSON, renderProjects } from '../global.js';
 const projectsContainer = document.querySelector('.projects');
 
 async function init() {
-  const projects = await fetchJSON('portfolio/lib/projects.json');
+  const projects = await fetchJSON('/portfolio/lib/projects.json');
   renderProjects(projects, projectsContainer, 'h2');
   const titleElement = document.querySelector('.projects-title');
 titleElement.textContent = `${projects.length} Projects`;
