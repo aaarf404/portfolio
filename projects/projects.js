686ee1a69c1dc31e6d74141e8b057d6b62b0d3f7
aaarf404 <alexmrf6439@gmail.com>
2025-05-02 20:58:58 -0700

    step 2.2

--- projects/projects.js
+++ projects/projects.js
@@ -12,11 +12,19 @@ titleElement.textContent = `${projects.length} Projects`;
 init();
 
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
-let data = [1, 2, 3, 4, 5];
+let data = [
+  { value: 1, label: 'apples' },
+  { value: 2, label: 'oranges' },
+  { value: 3, label: 'mangos' },
+  { value: 4, label: 'pears' },
+  { value: 5, label: 'limes' },
+  { value: 5, label: 'cherries' },
+];
+
 let colors = d3.scaleOrdinal(d3.schemeTableau10);
 
 let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
-let sliceGenerator = d3.pie();
+let sliceGenerator = d3.pie().value((d) => d.value);
 
 let arcData = sliceGenerator(data);
 let arcs = arcData.map((d) => arcGenerator(d));
@@ -26,4 +34,13 @@ arcs.forEach((arc, idx) => {
     .append('path')
     .attr('d', arc)
     .attr('fill', colors[idx]);
+});
+
+let legend = d3.select('.legend');
+
+data.forEach((d, idx) => {
+  legend
+    .append('li')
+    .attr('style', `--color:${colors(idx)}`)
+    .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`);
 });
