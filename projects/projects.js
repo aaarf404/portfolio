5e58ddae6d238ef600dee34ae6b4cb066da057cf
aaarf404 <alexmrf6439@gmail.com>
2025-05-02 20:55:01 -0700

    add pie chart figure

--- projects/projects.js
+++ projects/projects.js
@@ -10,3 +10,20 @@ titleElement.textContent = `${projects.length} Projects`;
 }
 
 init();
+
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
+let data = [1, 2];
+let colors = ['gold', 'purple'];
+
+let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+let sliceGenerator = d3.pie();
+
+let arcData = sliceGenerator(data);
+let arcs = arcData.map((d) => arcGenerator(d));
+
+arcs.forEach((arc, idx) => {
+  d3.select('svg')
+    .append('path')
+    .attr('d', arc)
+    .attr('fill', colors[idx]);
+});
