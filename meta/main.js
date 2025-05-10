46b0d8d8c74e942f6ada353dba7d099e9bfcf9ec
aaarf404 <alexmrf6439@gmail.com>
2025-05-09 22:37:05 -0700

    finish up scatter plot

--- meta/main.js
+++ meta/main.js
@@ -9,80 +9,105 @@ async function loadData() {
     date: new Date(row.date + 'T00:00' + row.timezone),
     datetime: new Date(row.datetime),
   }));
-
   return data;
 }
 
 function processCommits(data) {
-    return d3.groups(data, d => d.commit).map(([commit, lines]) => {
-      let first = lines[0];
-      let { author, date, time, timezone, datetime } = first;
-  
-      let ret = {
-        id: commit,
-        url: 'https://github.com/aaarf404/portfolio/commit/' + commit,
-        author,
-        date,
-        time,
-        timezone,
-        datetime,
-        hourFrac: datetime.getHours() + datetime.getMinutes() / 60,
-        totalLines: lines.length,
-      };
-  
-      Object.defineProperty(ret, 'lines', {
-        value: lines,
-        configurable: true,
-        writable: false,
-        enumerable: false,
-      });
-  
-      return ret;
+  return d3.groups(data, d => d.commit).map(([commit, lines]) => {
+    let first = lines[0];
+    let { author, date, time, timezone, datetime } = first;
+
+    let ret = {
+      id: commit,
+      url: 'https://github.com/aaarf404/portfolio/commit/' + commit,
+      author,
+      date,
+      time,
+      timezone,
+      datetime,
+      hourFrac: datetime.getHours() + datetime.getMinutes() / 60,
+      totalLines: lines.length,
+    };
+
+    Object.defineProperty(ret, 'lines', {
+      value: lines,
+      configurable: true,
+      writable: false,
+      enumerable: false,
     });
-  }
 
-  function renderCommitInfo(data, commits) {
-    const dl = d3.select('#stats').append('dl').attr('class', 'stats');
+    return ret;
+  });
+}
 
-    // num lines
-    dl.append('dt').html('Total <abbr title="Lines of code">LOC</abbr>');
-    dl.append('dd').text(data.length);
-  
-    //total commite
-    dl.append('dt').text('Total commits');
-    dl.append('dd').text(commits.length);
-  }  
+function renderCommitInfo(data, commits) {
+  const dl = d3.select('#stats').append('dl').attr('class', 'stats');
 
-  function renderScatterPlot(data, commits) {
-    const width = 1000;
-    const height = 600;
-  
-    const svg = d3.select('#chart')
-      .append('svg')
-      .attr('viewBox', `0 0 ${width} ${height}`)
-      .style('overflow', 'visible');
-    const xScale = d3.scaleTime()
-      .domain(d3.extent(commits, d => d.datetime))
-      .range([0, width])
-      .nice();
-  
-    const yScale = d3.scaleLinear()
-      .domain([0, 24])
-      .range([height, 0]);
+  dl.append('dt').html('Total <abbr title="Lines of code">Lines of Code</abbr>');
+  dl.append('dd').text(data.length);
 
-    const dots = svg.append('g').attr('class', 'dots');
-  
-    dots.selectAll('circle')
-      .data(commits)
-      .join('circle')
-      .attr('cx', d => xScale(d.datetime))
-      .attr('cy', d => yScale(d.hourFrac))
-      .attr('r', 5)
-      .attr('fill', 'steelblue');
-  }
-  
-let data = await loadData();
-let commits = processCommits(data);
+  dl.append('dt').text('Total commits');
+  dl.append('dd').text(commits.length);
+}
+
+function renderScatterPlot(data, commits) {
+  const width = 1000;
+  const height = 600;
+  const margin = { top: 10, right: 10, bottom: 30, left: 20 };
+
+  const usableArea = {
+    top: margin.top,
+    right: width - margin.right,
+    bottom: height - margin.bottom,
+    left: margin.left,
+    width: width - margin.left - margin.right,
+    height: height - margin.top - margin.bottom,
+  };
+
+  const svg = d3.select('#chart')
+    .append('svg')
+    .attr('viewBox', `0 0 ${width} ${height}`)
+    .style('overflow', 'visible');
+
+  const xScale = d3.scaleTime()
+    .domain(d3.extent(commits, d => d.datetime))
+    .range([usableArea.left, usableArea.right])
+    .nice();
+
+  const yScale = d3.scaleLinear()
+    .domain([0, 24])
+    .range([usableArea.bottom, usableArea.top]);
+  svg.append('g')
+    .attr('class', 'gridlines')
+    .attr('transform', `translate(${usableArea.left}, 0)`)
+    .call(d3.axisLeft(yScale)
+      .tickFormat('')
+      .tickSize(-usableArea.width)
+    );
+  const xAxis = d3.axisBottom(xScale);
+  const yAxis = d3.axisLeft(yScale)
+    .tickFormat(d => String(d % 24).padStart(2, '0') + ':00');
+
+  svg.append('g')
+    .attr('transform', `translate(0, ${usableArea.bottom})`)
+    .call(xAxis);
+
+  svg.append('g')
+    .attr('transform', `translate(${usableArea.left}, 0)`)
+    .call(yAxis);
+  const dots = svg.append('g').attr('class', 'dots');
+
+  dots.selectAll('circle')
+    .data(commits)
+    .join('circle')
+    .attr('cx', d => xScale(d.datetime))
+    .attr('cy', d => yScale(d.hourFrac))
+    .attr('r', 5)
+    .attr('fill', 'steelblue');
+}
+
+const data = await loadData();
+const commits = processCommits(data);
 console.log(commits);
 renderCommitInfo(data, commits);
 renderScatterPlot(data, commits);
