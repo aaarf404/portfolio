3aae0ab68d814636f7af3de9d54dd1de562088d0
aaarf404 <alexmrf6439@gmail.com>
2025-05-09 22:54:35 -0700

    Update main.js

--- meta/main.js
+++ meta/main.js
@@ -77,6 +77,12 @@ function renderScatterPlot(data, commits) {
   const yScale = d3.scaleLinear()
     .domain([0, 24])
     .range([usableArea.bottom, usableArea.top]);
+
+const [minLines, maxLines] = d3.extent(commits, d => d.totalLines);
+const rScale = d3.scaleSqrt()
+    .domain([minLines, maxLines])
+    .range([2, 30]);
+    
   svg.append('g')
     .attr('class', 'gridlines')
     .attr('transform', `translate(${usableArea.left}, 0)`)
@@ -95,23 +101,31 @@ function renderScatterPlot(data, commits) {
   svg.append('g')
     .attr('transform', `translate(${usableArea.left}, 0)`)
     .call(yAxis);
-    const dots = svg.append('g').attr('class', 'dots');
+
+const sortedCommits = d3.sort(commits, d => -d.totalLines);
+const dots = svg.append('g').attr('class', 'dots');
+dots.selectAll('circle')
+  .data(sortedCommits)
+
 
     dots.selectAll('circle')
       .data(commits)
       .join('circle')
       .attr('cx', d => xScale(d.datetime))
       .attr('cy', d => yScale(d.hourFrac))
-      .attr('r', 5)
+      .attr('r', d => rScale(d.totalLines))
       .attr('fill', 'steelblue')
+      .style('fill-opacity', 0.7)
       .on('mouseenter', (event, commit) => {
+        d3.select(event.currentTarget).style('fill-opacity', 1);
         renderTooltipContent(commit);
         updateTooltipVisibility(true);
         updateTooltipPosition(event);
       })
-      .on('mouseleave', () => {
+      .on('mouseleave', (event) => {
+        d3.select(event.currentTarget).style('fill-opacity', 0.7);
         updateTooltipVisibility(false);
-      });
+      })      
       
 }
 
